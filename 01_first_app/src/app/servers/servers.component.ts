3e80fd45b8794638a2780c041a0124e1b167939e
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-11-24 13:57:34 +0100

    all the selectors baby :)

--- 01_first_app/src/app/servers/servers.component.ts
+++ 01_first_app/src/app/servers/servers.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // selector: 'app-servers',
-  selector: '[app-servers]', // CSS attribute selector
-  // templateUrl: './servers.component.html',
+  selector: 'app-servers, [app-servers], .app-servers, #app-servers',
   template: `
 <app-server></app-server>
 <app-server></app-server>`,
