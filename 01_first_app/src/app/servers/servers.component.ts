4c9d068f26922a08a3cdfbd3826f4f5f5cb7e66d
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-11-24 13:05:47 +0100

    using attribute as selector instead of html/xml tag?

--- 01_first_app/src/app/servers/servers.component.ts
+++ 01_first_app/src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: 'app-servers',
+  selector: '[app-servers]', // CSS attribute selector
   // templateUrl: './servers.component.html',
   template: `
 <app-server></app-server>
