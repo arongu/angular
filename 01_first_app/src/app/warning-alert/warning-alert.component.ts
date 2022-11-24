aa5f05e0b20f94351955bd2f4f876ce698d27812
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-11-24 17:20:51 +0100

    my take on the first assignment

---
+++ 01_first_app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template:'<h1>WarningAlert</h1>',
+  styles: [`
+    h1 { background-color: red;color: white }
+  `]
+})
+
+export class WarningAlertComponent {
+}
