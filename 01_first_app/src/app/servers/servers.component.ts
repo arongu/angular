f3d17cb07926794c9e5d5c6a3b40af2a645153cc
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-11-25 10:50:52 +0100

    cleanup and event binding

--- 01_first_app/src/app/servers/servers.component.ts
+++ 01_first_app/src/app/servers/servers.component.ts
@@ -3,15 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers, [app-servers], .app-servers, #app-servers',
   templateUrl: './servers.component.html',
-//   template: `
-// <app-server></app-server>
-// <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreationStatus = "No server was created!";
 
   constructor() {
     setTimeout(() => this.allowNewServer = true, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!";
   }
 }
