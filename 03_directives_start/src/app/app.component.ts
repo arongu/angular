891cd9416953f5ed6cf854cad3ced48fcf66cb1e
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-12-20 15:46:46 +0100

    recap ngClass and ngStyle

--- 03_directives_start/src/app/app.component.ts
+++ 03_directives_start/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  numbers = [1, 2, 3, 4, 5];
+  even_numbers = [ 2, 4 ];
+  odd_numbers  = [ 1, 3, 5];
   onlyOdd = false;
 }
