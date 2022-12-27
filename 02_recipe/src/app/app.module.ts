be75b2641246e992ed9a6412b00647800e7b507e
Aron Gulyas <aron.gulyas.hu@gmail.com>
2022-12-27 05:58:39 +0100

    cleanup and dropdown fixed

--- 02_recipe/src/app/app.module.ts
+++ 02_recipe/src/app/app.module.ts
@@ -8,10 +8,10 @@ import {HeaderComponent} from './header/header.component';
 import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
 import {RecipeItemComponent} from './recipes/recipe-item/recipe-item.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeComponent} from './recipes/recipe/recipe.component';
 import {ShoppingListEditComponent} from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import {ShoppingListIngerdientComponent} from './shopping-list/shopping-list-ingerdient/shopping-list-ingerdient.component';
 import {FormsModule} from "@angular/forms";
+import {DropdownDirective} from "./shared/dropdown.directive";
 
 @NgModule({
   declarations: [
@@ -22,9 +22,9 @@ import {FormsModule} from "@angular/forms";
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    RecipeComponent,
     ShoppingListEditComponent,
-    ShoppingListIngerdientComponent
+    ShoppingListIngerdientComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
--- 02_recipe/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ 02_recipe/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
-import {Ingredient} from "../../shared/ingridient.model";
+import {Ingredient} from "../../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list.service";
 
 @Component({
